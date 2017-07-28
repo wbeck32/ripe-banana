ea3727158490ef539f4b50023fe06117703ec78c
Wendy Beck <webeck@gmail.com>
2017-07-27 17:12:51 -0700

    adding directory structure

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+// this will cause mongoose to make the db connection
+require("./lib/connect");
+const app = require("./lib/app");
+
+const server = http.createServer(app);
+
+const port = 3000;
+
+server.listen(port, () => {
+  // eslint-disable-next-line
+  console.log("server up and running on", server.address().port);
+});
