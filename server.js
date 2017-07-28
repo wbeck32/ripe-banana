82fdadfacf9a024a66f1ffd96941a3b432549d45
christylaguardia <christinelaguardia@gmail.com>
2017-07-28 15:19:37 -0700

    replaced double quotes with single

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http = require("http");
+const http = require('http');
 // this will cause mongoose to make the db connection
-require("./lib/connect");
-const app = require("./lib/app");
+require('./lib/connect');
+const app = require('./lib/app');
 
 const server = http.createServer(app);
 
@@ -9,5 +9,5 @@ const port = 3000;
 
 server.listen(port, () => {
   // eslint-disable-next-line
-  console.log("server up and running on", server.address().port);
+  console.log('server up and running on', server.address().port);
 });
