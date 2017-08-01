c69dc3fe1a0a07ecc5276350f7b04f749de5070a
Wendy Beck <webeck@gmail.com>
2017-07-31 17:17:38 -0700

    fixing merge

--- tests/e2e/actor-e2e-tests.js
+++ tests/e2e/actor-e2e-tests.js
@@ -11,8 +11,11 @@ const connection = require('mongoose').connection;
 const app = require('../../lib/app');
 const request = chai.request(app);
 
-describe.skip('actor e2e tests', () => {
+const Studio = require('../../lib/models/studio-model');
+const Actor = require('../../lib/models/actor-model');
+const Film = require('../../lib/models/film-model');
 
+describe('actor e2e tests', () => {
 
     before(() => connection.dropDatabase());
 
@@ -40,63 +43,68 @@ describe.skip('actor e2e tests', () => {
         pob: 'San Francisco, California, U.S.'
     });
 
-    let films = {
-        dragon1: {
-            title: 'Way of the Dragon',
-            studio: studios.golden._id,
-            released: 1972,
-            cast: [
-                {
-                    role: 'Tang Lung',
-                    actor: actors.bruce._id
-                },{
-                    role: 'Chen Ching-hua',
-                    actor: actors.nora._id
-                },{
-                    role: 'Ho',
-                    actor: actors.paul._id
-                }
-            ]
-        },
-        dragon2: {
-            title: 'Enter the Dragon',
-            studio: studios.warner._id,
-            released: 1973,
-            cast: [
-                {
-                    role: 'Lee',
-                    actor: actors.bruce._id
-                },{
-                    role: 'Roper',
-                    actor: actors.john._id
-                },{
-                    role: 'Williams',
-                    actor: actors.jim._id
-                }
-            ]
-        },
-        fury: {
-            title: 'Fist of Fury',
-            studio: studios.golden._id,
-            released: 1972,
-            cast: [
-                {
-                    role: 'Chen Zhen',
-                    actor: actors.bruce._id
-                },{
-                    role: ' Yuan Li\'er',
-                    actor: actors.nora._id
-                },{
-                    role: 'Hiroshi Suzuki',
-                    actor: actors.riki._id
-                },{
-                    role: 'Petrov',
-                    actor: actors.robert._id
-                }
-            ]
-        }
-    };
 
+    let john = new Actor ({
+        name: 'John Saxon',
+        dob: new Date(1935, 7, 5),
+        pob: 'Brooklyn, New York City, New York, U.S.'
+    });
+
+    let jim = new Actor ({
+        name: 'Jim Kelly',
+        dob: new Date(1946, 4, 5),
+        pob: 'Millersburg, Kentucky, U.S.'
+    });
+
+    let nora = new Actor ({
+        name: 'Nora Miao',
+        dob: new Date(1952, 1, 8),
+        pob: 'Hong Kong'
+    });
+
+    let riki = new Actor ({ name: 'Riki Hashimoto' });
+
+    let robert = new Actor ({ name: 'Robert Baker' });
+
+    let paul = new Actor ({
+        name: 'Paul Wei',
+        dob: new Date(1929, 10, 29),
+        pob: 'Nanjing, China'
+    });
+
+    let dragon1 = new Film ({
+        title: 'Way of the Dragon',
+        studio: golden._id,
+        released: 1972,
+        cast: [
+            { role: 'Tang Lung', actor: bruce._id },
+            { role: 'Chen Ching-hua', actor: nora._id },
+            { role: 'Ho', actor: paul._id }
+        ]
+    });
+
+    let dragon2 = new Film ({
+        title: 'Enter the Dragon',
+        studio: warner._id,
+        released: 1973,
+        cast: [
+            { role: 'Lee', actor: bruce._id },
+            { role: 'Roper', actor: john._id },
+            { role: 'Williams', actor: jim._id }
+        ]
+    });
+
+    let fury = new Film ({
+        title: 'Fist of Fury',
+        studio: golden._id,
+        released: 1972,
+        cast: [
+            { role: 'Chen Zhen', actor: bruce._id },
+            { role: 'Yuan Li\'er', actor: nora._id },
+            { role: 'Hiroshi Suzuki', actor: riki._id },
+            { role: 'Petrov', actor: robert._id }
+        ]
+    });
 
     function saveStudio(studio) {
         return request.post('/studios')
@@ -141,24 +149,6 @@ describe.skip('actor e2e tests', () => {
             .then(saveStudio(golden))
             .then(saveFilm(dragon1))
             .then(saveFilm(fury));
-
-        saveActor(actors.bruce).then(saved => actors.bruce = saved)
-            .then(saveActor(actors.john).then(saved => actors.john = saved))
-            .then(saveActor(actors.jim).then(saved => actors.jim = saved))
-            .then(saveActor(actors.nora).then(saved => actors.nora = saved))
-            .then(saveActor(actors.riki).then(saved => actors.riki = saved))
-            .then(saveActor(actors.robert).then(saved => actors.robert = saved))
-            .then(saveActor(actors.paul).then(saved => actors.paul = saved))
-
-
-            .then(saveStudio(studios.warner)).then(saved => studios.warner = saved)
-            .then(saveFilm(films.dragon2)).then(saved => films.dragon2 = saved)
-
-            .then(saveStudio(studios.golden)).then(saved => studios.golden = saved)
-            .then(saveFilm(films.dragon1)).then(saved => films.dragon1 = saved)
-            .then(saveFilm(films.fury)).then(saved => films.fury = saved);
-
-            // console.log('actors>>>>>>>>', actors);
 
     });
 
