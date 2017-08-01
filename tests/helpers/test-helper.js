159fb474bfd7fc122b7800bc33226d7a766baa99
Wendy Beck <webeck@gmail.com>
2017-08-01 14:49:25 -0700

    removing chai assert

--- tests/helpers/test-helper.js
+++ tests/helpers/test-helper.js
@@ -1,6 +1,5 @@
 const app = require('../../lib/app');
 const chai = require('chai');
-const assert = chai.assert;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const request = chai.request(app);
