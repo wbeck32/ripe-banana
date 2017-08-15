4605deac05c1447512d6cd147c23bd28ed0b99f0
Wendy Beck <webeck@gmail.com>
2017-08-14 17:39:59 -0700

    committing unsaved changes to start this again

--- tests/e2e/film-e2e-tests.js
+++ tests/e2e/film-e2e-tests.js
@@ -6,93 +6,96 @@ chai.use(chaiHttp);
 const testHelper = require('../helpers/test-helper');
 
 describe('film e2e tests', () => {
+  const req = chai.request(app);
 
-    const req = chai.request(app);
+  let testStudio = testHelper.studio;
+  let testActor = testHelper.actor;
+  let testFilm = testHelper.film;
+  let testReview = testHelper.review;
+  let testReviewer = testHelper.reviewer;
 
-    let testStudio = testHelper.studio;
-    let testActor = testHelper.actor;
-    let testFilm = testHelper.film;
-    let testReview = testHelper.review;
-    let testReviewer = testHelper.reviewer;
+  before(() => {
+    //function to save multiple films
+    //function to save multiple reviews
+    // function to save multiple actors
+  });
 
-    before(() => {
-        //function to save multiple films
-        //function to save multiple reviews
-        // function to save multiple actors
-    });
+  it.only('POST /film', () => {
+        // console.log('---- IN TEST ----');
+        // console.log('testStudio: ', testStudio);
+        // console.log('testActor: ', testActor);
+        // console.log('testFilm: ', testFilm);
+        // console.log('testReviewer: ', testReviewer);
+        // console.log('testReview: ', testReview);
 
-    it('POST /film', () => {
 
-
-        // return req.post('/films')
-        //     .send(testFilm)
-        //     .then(savedFilm => {
-        //         console.log(savedFilm);
-        //     });
-    }), it('GET /films', () => {
-        //     // TODO: don't create consts as objects
-        //     console.log('1: ', testFilms);
-        //     // let filmName = Object.entries(testFilms)
-        //     //     .map(function(film) {
-        //     //         return film[1][Object.getOwnPropertyNames(film[1])];
-        //     //     });
-        //     return Promise.all(testFilms.forEach(film, () => {
-        //             console.log('in save film: ', film);
-        //             return save(film);
-        //         }))
-        //         .then(saved => {
-        //             console.log(typeof saved, saved);
-        //             testFilms = saved.forEach((item) => {
-        //                 console.log(item);
-        //             });
-        //             console.log('2.85: ', testFilms);
-
-        //             // .map((f) => {
-        //             // testFilms = f;
-        //             // console.log(f);
-
-        //             // testFilms : saved;
-        //             // console.log('2.85: ', testFilms);
-        //             return req.get('/films')
-        //                 .then(films => {
-        //                     // console.log('body: ',films.body);
-        //                     // console.log('tf: ',testFilms)
-        //                     assert.equal(3, films.body.length);
-        //                     // assert.deepEqual(films.body, testFilms);
-        //                 });
-        //         });
-
-    }), it('GET /film by id', () => {
-        //     let filmId = null;
-        //     return save(testFilm)
-        //         .then(savedFilm => {
-        //             filmId = '_' + savedFilm._id;
-        //             return req.get('/films')
-        //                 .query({ id: filmId })
-        //                 .then(res => {
-        //                     assert.equal(body.body[0]._id, savedFilm._id);
-        //                 });
-        //         });
-    }), it('DELETE /films by id', () => {
-        //     let filmId = null;
-        //     return save(testFilm)
-        //         .then(savedFilm => {
-        //             filmId = '_' + savedFilm._id;
-        //             return req.del('/films')
-        //                 .query({ id: filmId })
-        //                 .then(res => {
-        //                     assert.equal(body.statusCode, 200);
-        //                 });
-        //         });
-    }), it('PATCH /films', () => {
-        //     return save(testFilm)
-        //         .then(savedFilm => {
-        //             return req.patch('/films')
-        //                 .send({ id: savedFilm._id, newTitle: 'The Worst Film Ever' })
-        //                 .then(res => {
-        //                     assert.equal(body.statusCode, 200);
-        //                     assert.equal(body.body.title, 'The Worst Film Ever');
-        //                 });
-        //         });
-    });
+    // return req.post('/films')
+    //     .send(testFilm)
+    //     .then(savedFilm => {
+    //         console.log(savedFilm);
+    //     });
+  }), it('GET /films', () => {
+    // don't bother with an object, use an array
+    //     // TODO: don't create consts as objects
+    //     console.log('1: ', testFilms);
+    //     // let filmName = Object.entries(testFilms)
+    //     //     .map(function(film) {
+    //     //         return film[1][Object.getOwnPropertyNames(film[1])];
+    //     //     });
+    //     return Promise.all(testFilms.forEach(film, () => {
+    //             console.log('in save film: ', film);
+    //             return save(film);
+    //         }))
+    //         .then(saved => {
+    //             console.log(typeof saved, saved);
+    //             testFilms = saved.forEach((item) => {
+    //                 console.log(item);
+    //             });
+    //             console.log('2.85: ', testFilms);
+    //             // .map((f) => {
+    //             // testFilms = f;
+    //             // console.log(f);
+    //             // testFilms : saved;
+    //             // console.log('2.85: ', testFilms);
+    //             return req.get('/films')
+    //                 .then(films => {
+    //                     // console.log('body: ',films.body);
+    //                     // console.log('tf: ',testFilms)
+    //                     assert.equal(3, films.body.length);
+    //                     // assert.deepEqual(films.body, testFilms);
+    //                 });
+    //         });
+  }), it('GET /film by id', () => {
+    //     let filmId = null;
+    //     return save(testFilm)
+    //         .then(savedFilm => {
+    //             filmId = '_' + savedFilm._id;
+    //             return req.get('/films')
+    //                 .query({ id: filmId })
+    //                 .then(res => {
+    //                     assert.equal(body.body[0]._id, savedFilm._id);
+    //                 });
+    //         });
+  }), it('DELETE /films by id', () => {
+    //     let filmId = null;
+    //     return save(testFilm)
+    //         .then(savedFilm => {
+    //             filmId = '_' + savedFilm._id;
+    //             return req.del('/films')
+    //                 .query({ id: filmId })
+    //                 .then(res => {
+    //                     assert.equal(body.statusCode, 200);
+    //                 });
+    //         });
+  }), it('PATCH /films', () => {
+    //     return save(testFilm)
+    //         .then(savedFilm => {
+    //             return req.patch('/films')
+    //                 .send({ id: savedFilm._id, newTitle: 'The Worst Film Ever' })
+    //                 .then(res => {
+    //                     assert.equal(body.statusCode, 200);
+    //                     assert.equal(body.body.title, 'The Worst Film Ever');
+    //                 });
+    //         });
+  });
 });
